9e3e92c89c0aa26041bf649d308e9bfec9b4f991
Tyler Peterson <trpeterson42@gmail.com>
2017-06-26 19:42:02 -0700

    new

--- liri.js
+++ liri.js
@@ -2,72 +2,85 @@ var keys = require('./keys.js');
 
 var Twitter = require('twitter');
 
-var spotify = require('node-spotify-api');
+var Spotify = require('node-spotify-api');
 
 var request = require('request');
 
-var getMyTweets = function(){
-	
-	var client = new Twitter(keys.twitterKeys);
+var fs = require('fs');
+  
+	var getMyTweets = function(){
+		
+		var client = new Twitter(keys.twitterKeys);
 
-	var params = {screen_name: 'John Cina'};
-	client.get('statuses/user_timeline', params, function(error, tweets, response) {
-		if (!error) {
-		   	for (var i = 0; i < tweets.length; i++) {
-		   		console.log(tweets[i].created_at);
-		   		console.log(' ');
-		   		console.log(tweets[i].text);
-	   		}
-		}
-	});
-}
+		var params = {screen_name: 'John Cina'};
+		client.get('statuses/user_timeline', params, function(error, tweets, response) {
+			if (!error) {
+			   	for (var i = 0; i < tweets.length; i++) {
+			   		console.log(tweets[i].created_at);
+			   		console.log(' ');
+			   		console.log(tweets[i].text);
+		   		}
+			}
+		});
+	}
 
-var getArtistName = function(artist) {
-	return artist.name;
-}
+	var getArtistName = function(artist) {
+		return artist.name;
+	}
 
 var getMySpotify = function(songName) {
-	spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+	
+	var songName = process.argv[3];
+
+	var spotify = new Spotify({id: '08798a5a78d64c4aad19751f5321e489',
+  secret: '6aa10c6dce584a60b5d78423c0f384bb'});
+
+	spotify.search({ type: 'track', query: songName }, function(err, data) {
 	  	if (err) {
 	    	console.log('Error occurred: ' + err);
 	    	return;
 	  	}
-		var songs = data.tracks.items;
-		for (var i = 0; i < songs.length; i++) {
-			console.log(i)
-			console.log('artist(s): ' + songs[i].artist.map(getArtistName));
-			console.log('song name: ' + songs[i].name);
-			console.log('preview song: ' + songs[i].preview_url);				
-			console.log('album: ' + songs[i].album.name);
-			console.log('-------------------------------------------');
-		}		
+		var songs = data.tracks.items[0];
+			console.log('artist(s): ' + songs.artists[0].name);
+			console.log('song name: ' + songs.name);
+			console.log('preview song: ' + songs.preview_url);				
+			console.log('album: ' + songs.album.name);
+			console.log('-------------------------------------------');		
 	});
 }
 
-var getMoieFunction = function(movieName) {
-	request('http://http://www.omdbapi.com/?t=' + movieName + '&y=&plot=short&r=json',function(error, response, body){
+var getMovieFunction = function(movieName) {
+	
+	var movieName = process.argv[3];
+
+	request('http://www.omdbapi.com/?apikey=40e9cece&t=' + movieName + '&y=&plot=short&r=json',function(error, response, body){
 		if (!error && response.statusCode == 200) {
-			console.log('Title: ' + jsonData.Title);
-			console.log('Year: ' + jsonData.Year);
-			console.log('Rated: ' + jsonData.Rated);
-			console.log('IMDB Ratings: ' + jsonData.imdbRating);
-			console.log('Country: ' + jsonData.Country);
-			console.log('Plot: ' + jsonData.Plot);
+			console.log('Title: ' + JSON.parse(body).Title);
+			console.log('Year: ' + JSON.parse(body).Year);
+			console.log('Rated: ' + JSON.parse(body).Rated);
+			console.log('IMDB Ratings: ' + JSON.parse(body).imdbRating);
+			console.log('Country: ' + JSON.parse(body).Country);
+			console.log('Plot: ' + JSON.parse(body).Plot);
+			console.log('Language: ' + JSON.parse(body).Language);
+			console.log('Country: ' + JSON.parse(body).Country);
+			console.log('Actors: ' + JSON.parse(body).Actors);
+			console.log('Rotten Tomatoes: ' + JSON.parse(body).Ratings[1].Value);
 		}
 	});
 }
 var doWhatItSays = function(caseData, functionData) {
 	fs.readFile('random.txt','utf8', function(err, data) {
-		if (err) throw err;
+		// if (err) throw err;
 		
-		var dataArr.length = data.split(',');
+		var dataArr = data.split(',');
 
 		if (dataArr.length == 2) {
 			pick(dataArr[0], dataArr[1]);
 		}
-		else if {(dataArr.length == 1)
+		else if (dataArr.length == 1){
 			pick(dataArr[0]);
 		}
+	
 	});
 }
 var pick = function(caseData, functionData) {
@@ -79,7 +92,7 @@ var pick = function(caseData, functionData) {
 			getMySpotify();
 			break; 
 		case 'movie-this' :
-			getMySpotify();
+			getMovieFunction();
 		case 'do-what-it-says' :
 			doWhatItSays();
 			break;
